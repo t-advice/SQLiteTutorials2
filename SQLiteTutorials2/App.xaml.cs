7e69b2b0f2e8bd17e513190bb595f87e46b54e04
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-14 10:29:02 +0200

    global access database set up completed , ui in mainpage completed,

--- SQLiteTutorials2/App.xaml.cs
+++ SQLiteTutorials2/App.xaml.cs
@@ -1,15 +1,33 @@
-﻿namespace SQLiteTutorials2
+﻿using SQLiteTutorials2.Services;
+
+
+
+namespace SQLiteTutorials2
 {
     public partial class App : Application
     {
+        static DatabaseService _database;
+        public static DatabaseService Database
+        {
+            get
+            {
+                if (_database == null)
+                { 
+                    string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Tasks.db3");
+                    _database = new DatabaseService(dbPath);
+                }
+                return _database;
+            
+            }
+        }
+
+
         public App()
         {
             InitializeComponent();
+            MainPage = new AppShell();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        
     }
 }
