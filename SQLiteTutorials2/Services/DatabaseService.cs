cd354cb8e12045845ac7db05a2b93f5864f9f89d
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-14 10:19:29 +0200

    database service class created with get , save and delete tasks synced.

--- SQLiteTutorials2/Services/DatabaseService.cs
+++ SQLiteTutorials2/Services/DatabaseService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SQLite;
+using SQLiteTutorials2.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,14 @@ namespace SQLiteTutorials2.Services
 {
     internal class DatabaseService
     {
+        private readonly SQLiteAsyncConnection _database;
+        public DatabaseService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<TaskItem>().Wait();
+        }
+        public Task<List<TaskItem>> GetTaskItemAsync() => _database.Table<TaskItem>().ToListAsync();
+        public Task<int> SaveTaskAsync(TaskItem task) => _database.InsertOrReplaceAsync(task);
+        public Task<int> DeleteTaskAsync(TaskItem task) => _database.DeleteAsync(task);
     }
 }
